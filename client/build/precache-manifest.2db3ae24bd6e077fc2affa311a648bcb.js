0ddfc73ab5b4d6a4cbf0b9460fc9442ae44c16b4
kawsarbyherself <kawsar_shafi@yahoo.com>
2021-12-12 21:26:59 -0800

    build

---
+++ client/build/precache-manifest.2db3ae24bd6e077fc2affa311a648bcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f20647e3b195d84c425b8a8b6913a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941c8ca04ff190d953e8",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "5f69ea4e1595d8d74660",
+    "url": "/static/js/2.4d6c96b3.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.4d6c96b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "941c8ca04ff190d953e8",
+    "url": "/static/js/main.4314d6d6.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -19,8 +19,16 @@ const PORT = process.env.PORT|| 5000;
 
 const CONNECTION_URL = 'mongodb+srv://kawsarbyherself:Appleinc24@cluster0.5ragz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
